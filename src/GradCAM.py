1c4d114553a689170e81fa2dddbe4db2875beb7b
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-07-15 20:14:06 +0200

    generic refactor

--- src/GradCAM.py
+++ src/GradCAM.py
@@ -1,117 +1,81 @@
+import cv2
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
 from torch.utils import data
 from torchvision import datasets
-import numpy as np
 from Models import ResNet, VGG
-import matplotlib.pyplot as plt
+from os import makedirs
+from os.path import exists
+from PIL import Image
 
-def grad_cam(model):
-
-    if model == "resnet50":
+def grad_cam(model_name, img_path):
+    # choose working model
+    if model_name == "resnet50":
         # define model
-        resnet = ResNet()
-        # set the evaluation mode
-        resnet.eval()
-
-        # define 1 image dataset
-        dataset = datasets.ImageFolder(root='./data/', transform=resnet.transforms)
-        dataloader = data.DataLoader(dataset=dataset, shuffle=False, batch_size=1)
-        # predict image
-        img, _ = next(iter(dataloader))
-        pred = resnet(img)
-        print("Output label:",pred.argmax(dim=1))
-        #pred_back = pred[0].backward()
-
-        # backward pass
-        pred[:, 0].backward()
-        # pull the gradients out of the model
-        gradients = resnet.get_activations_gradient()
-        print("gradients shape:",gradients.shape)
-        # pool the gradients across the channels
-        pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
-        # get the activations of the last convolutional layer
-        activations = resnet.get_activations(img).detach()
-        print("activations shape:",activations.shape)
-        # weight the channels by corresponding gradients
-        for i in range(512):
-            activations[:, i, :, :] *= pooled_gradients[i]
-            
-        # average the channels of the activations
-        heatmap = torch.mean(activations, dim=1).squeeze()
-
-        # relu on top of the heatmap
-        # expression (2) in https://arxiv.org/pdf/1610.02391.pdf
-        heatmap = np.maximum(heatmap, 0)
-
-        # normalize the heatmap
-        heatmap /= torch.max(heatmap)
-
-        # draw the heatmap
-        #plt.matshow(heatmap.squeeze())
-        #plt.savefig('test.png')
-
-        # create superimposed image
-        import cv2
-        img = cv2.imread('data/tench/2.JPEG')
-        heatmap = cv2.resize(heatmap.numpy(), (img.shape[1], img.shape[0]))
-        heatmap = np.uint8(255 * heatmap)
-        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-        superimposed_img = heatmap * 0.4 + img
-        cv2.imwrite('./map.jpg', superimposed_img)
-
-    if model == "vgg19":
+        model = ResNet()
+    elif model_name == "vgg19":
         # define model
-        vgg = VGG()
-        # set the evaluation mode
-        vgg.eval()
+        model = VGG()
+    else:
+        exit("Please choose a valid model.")
+    print(f"Model: {model_name}")
 
-        # define 1 image dataset
-        dataset = datasets.ImageFolder(root='./data/', transform=vgg.transforms)
-        dataloader = data.DataLoader(dataset=dataset, shuffle=False, batch_size=1)
-        # get the image from the dataloader
-        img, label = next(iter(dataloader))
-        # get the most likely prediction of the model
-        pred = vgg(img)
+    # check input folder exists
+    # if not exists('./data'):
+        # exit("Please create a folder named 'data' with inside one folder named 'test' with 1 image.")
 
-        # backward pass
-        pred[:, 0].backward()
-        # pull the gradients out of the model
-        gradients = vgg.get_activations_gradient()
-        print("gradients shape:",gradients.shape)
-        # pool the gradients across the channels
-        pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
-        # get the activations of the last convolutional layer
-        activations = vgg.get_activations(img).detach()
-        print("activations shape:",activations.shape)
-        # weight the channels by corresponding gradients
-        for i in range(512):
-            activations[:, i, :, :] *= pooled_gradients[i]
-            
-        # average the channels of the activations
-        heatmap = torch.mean(activations, dim=1).squeeze()
+    # set the evaluation mode
+    model.eval()
+    # define 1 image dataset
+    # dataset = datasets.ImageFolder(root='./data/', transform=model.transforms)
+    # dataloader = data.DataLoader(dataset=dataset, shuffle=False, batch_size=1)
 
-        # relu on top of the heatmap
-        # expression (2) in https://arxiv.org/pdf/1610.02391.pdf
-        heatmap = np.maximum(heatmap, 0)
+    # open image
+    img = Image.open(img_path)
+    # preprocess image
+    img = model.transforms(img)
+    # add batch dimension
+    img = torch.unsqueeze(img, dim=0)
+    # predict image
+    pred = model(img)
+    print("Predicted image label:",pred.argmax(dim=1))
+    # backward pass
+    pred[:, 0].backward()
+    # get the gradients
+    gradients = model.get_activations_gradient()
+    print("gradients shape:",gradients.shape)
+    # pool gradients across channels
+    pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
+    # get the activations until the last convolutional layer
+    activations = model.get_activations(img).detach()
+    print("activations shape:",activations.shape)
+    # weight the channels by corresponding gradients
+    for i in range(512):
+        activations[:, i, :, :] *= pooled_gradients[i]
+    # average the channels of the activations
+    heatmap = torch.mean(activations, dim=1).squeeze()
 
-        # normalize the heatmap
-        heatmap /= torch.max(heatmap)
+    # expression (2) from https://arxiv.org/pdf/1610.02391.pdf
+    heatmap = np.maximum(heatmap, 0)
 
-        # draw the heatmap
-        #plt.matshow(heatmap.squeeze())
-        #plt.savefig('test.png')
+    # normalize the heatmap
+    heatmap /= torch.max(heatmap)
 
-        # create superimposed image
-        import cv2
-        img = cv2.imread('data/tench/2.JPEG')
-        heatmap = cv2.resize(heatmap.numpy(), (img.shape[1], img.shape[0]))
-        heatmap = np.uint8(255 * heatmap)
-        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-        superimposed_img = heatmap * 0.4 + img
-        cv2.imwrite('./map.jpg', superimposed_img)
+    # control that results directory exists
+    out_dir = './results'
+    if not exists(out_dir):
+        makedirs(out_dir)
 
-        print("Output label:",pred.argmax(dim=1))
-
+    # create superimposed image
+    img = cv2.imread(img_path)
+    heatmap = cv2.resize(heatmap.numpy(), (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    superimposed_img = heatmap * 0.4 + img
+    cv2.imwrite(f'./{out_dir}/map.jpg', superimposed_img)
+    cv2.imwrite(f'./{out_dir}/heatmap.jpg', heatmap)
+    
 if __name__ == "__main__":
-    grad_cam("resnet50")
-    # grad_cam("vgg19")
+    grad_cam("resnet50","./data/test/2.JPEG")
+    # grad_cam("vgg19","./data/test/2.JPEG")
--- src/Models.py
+++ src/Models.py
@@ -1,33 +1,51 @@
 import torch
 from torch import nn
 
-class ResNet(nn.Module):
+class GenericModel(nn.Module):
+    def __init__(self):
+        super(GenericModel, self).__init__()
+        self.gradients = None
+
+    def forward(self):
+        pass
+
+    def get_activations(self, x):
+        """ Should pass input x through all layers before the classification head.
+            Must be redefined for each specific model.
+        """
+        pass
+
+    def activations_hook(self, grad):
+        """ Function to call whenever the 
+            gradient with respecto to the Tensor is computed.
+
+            In our specific case we simply copy the gradients
+        """
+        self.gradients = grad
+
+    def get_activations_gradient(self):
+        return self.gradients
+
+    
+class ResNet(GenericModel):
     def __init__(self):
         super(ResNet, self).__init__()
-        from torchvision.models import resnet50, ResNet50_Weights
-
         # define resnet and weights
+        from torchvision.models import resnet50, ResNet50_Weights
         self.weights = ResNet50_Weights.DEFAULT
         self.resnet = resnet50(weights=self.weights)
-        print("self",self)
-        print()
 
-        # model preprocessing
+        # model preprocessing of input images
         self.transforms = self.weights.transforms()
 
         # placeholder for the gradients
         self.gradients = None
 
-    def activations_hook(self, grad):
-        self.gradients = grad
-
     def forward(self, x):
         # model before classification head
         x = self.get_activations(x)
-
         # register the hook
         h = x.register_hook(self.activations_hook)
-
         # avg pooling + classification head
         x = self.resnet.avgpool(x)
         x = x.view((1, 2048))
@@ -35,13 +53,9 @@ class ResNet(nn.Module):
 
         return x
 
-
-
-    def get_activations_gradient(self):
-        return self.gradients
-
-    # method for the activation exctraction
     def get_activations(self, x):
+        """ Method for the activation exctraction
+        """
         x = self.resnet.conv1(x)
         x = self.resnet.bn1(x)
         x = self.resnet.relu(x)
@@ -53,7 +67,7 @@ class ResNet(nn.Module):
         return x
 
 
-class VGG(nn.Module):
+class VGG(GenericModel):
     def __init__(self):
         super(VGG, self).__init__()
         from torchvision.models import vgg19, VGG19_Weights
@@ -62,26 +76,22 @@ class VGG(nn.Module):
         self.weights = VGG19_Weights.DEFAULT
         self.vgg = vgg19(weights=self.weights)
         
-        # disect the network to access its last convolutional layer
+        # all layers before classification head
         self.features_conv = self.vgg.features[:36]
         
-        # get the max pool of the features stem
+        # get the max pool before classification head
         self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
         
         # get the classifier of the vgg19
         # it is simply the last sequential block
         self.classifier = self.vgg.classifier
-        print("VGG classifier:",self.classifier)
         
         # placeholder for the gradients
         self.gradients = None
     
-        # model preprocessing
+        # model preprocessing for images
         self.transforms = self.weights.transforms()
 
-    # hook for the gradients of the activations
-    def activations_hook(self, grad):
-        self.gradients = grad
         
     def forward(self, x):
         x = self.features_conv(x)
@@ -94,10 +104,6 @@ class VGG(nn.Module):
         x = x.view((1, -1))
         x = self.classifier(x)
         return x
-    
-    # method for the gradient extraction
-    def get_activations_gradient(self):
-        return self.gradients
     
     # method for the activation exctraction
     def get_activations(self, x):
